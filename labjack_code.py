24a95024780caa8485c82b4f0ea242d895101b1b
sr3srgang <77465532+sr3srgang@users.noreply.github.com>
2025-01-15 19:54:04 -0700

    new

--- labjack_code.py
+++ labjack_code.py
@@ -8,8 +8,17 @@ class LabJackT7:
     def read_voltage_AIN0(self):
         handle = ljm.openS("T7", "ETHERNET", self.ip_address)
         voltage = ljm.eReadName(handle, "AIN0")
-        print(f"Voltage at AIN0: {voltage:.4f} V")
+        print(f"volts: {voltage:.4f} V")
         return voltage
+
+    def read_flow(self):
+        print("Inside read_flow method...")
+        volts = self.read_voltage_AIN0()
+        print(f"Voltage read: {volts:.4f}")
+        flow = volts * 0.25 + 0.25
+        print(f"Calculated flow: {flow:.4f}")
+        return flow
+
 
 #out = LabJackT7("192.168.1.120").read_voltage_AIN0()
 
--- run_flowloop.py
+++ run_flowloop.py
@@ -1,13 +1,13 @@
 import numpy as np
-from FlowLoop import FlowLoop
+from FlowLoop2 import FlowLoop
 import sys
 from PyQt5.QtWidgets import QApplication
-from labjack_code import abjack_code
+from labjack_code import LabJackT7
 
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
-    l = labjack_code('192.168.1.120')
-    loop=TempLoop("Flow", l, 10)
+    l = LabJackT7('192.168.1.92')
+    loop=FlowLoop("Flow", l, 5)
     loop.show()
     sys.exit(app.exec_())
